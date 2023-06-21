5b88e729e235a3506ec6ca277970e03a2cc888fb
Stephen <mbuguagichimu@gmail.com>
2023-06-21 10:37:50 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,52 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table, starting with 0
- * @n: Number of the times table
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ *
+ * Description: This function prints the times table of the given number n,
+ * starting from 0 up to n.
  */
 void print_times_table(int n)
 {
+	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
-		int i, j;
 		for (i = 0; i <= n; i++)
 		{
-			print_times_row(i, n);
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+					_putchar(k + '0');
+				else if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * print_times_row - Prints a row of the times table
- * @row: Row number
- * @n: Number of the times table
- */
-void print_times_row(int row, int n)
-{
-	int j, k;
-
-	for (j = 0; j <= n; j++)
-	{
-		k = j * row;
-		if (j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			if (k < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (k < 100)
-			{
-				_putchar(' ');
-			}
-		}
-		_putchar(k + '0');
 	}
 }
 
