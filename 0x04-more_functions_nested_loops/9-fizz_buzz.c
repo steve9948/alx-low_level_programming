90c7363f8361f6a0502b158a95856df496f88191
Stephen <mbuguagichimu@gmail.com>
2023-06-22 12:01:14 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,29 +12,28 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0 && a % 5 != 0)
+		if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (a % 5 == 0 && a % 3 != 0)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (a % 3 == 0 && a % 5 == 0)
+		else if ((a % 3 == 0) && (a % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if (a == 1)
+		else
 		{
 			printf("%d", a);
 		}
-		else
+
+		if (a != 100)
 		{
-			printf("%d ", a);
+			printf(" ");
 		}
 	}
-
 	printf("\n");
-
 	return (0);
 }
