e6525bc476839c55094694cc8844b50229a158f1
Stephen <mbuguagichimu@gmail.com>
2023-06-22 14:38:13 -0700

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
  * print_triangle - prints a triangle, followed by a new line
- *
+ * @size: size of the triangle
  * Return: Always 0
  */
 void print_triangle(int size)
@@ -31,3 +31,4 @@ void print_triangle(int size)
         }
     }
 }
+
