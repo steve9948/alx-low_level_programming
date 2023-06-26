382633848f0bad48889c2fbff21bdbb6d1a7bfd8
Stephen <mbuguagichimu@gmail.com>
2023-06-26 10:41:20 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _stoi - used to convert a string to an integer
+ * _atoi - used to convert a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-int _stoi(char *s)
+int _atoi(char *s)
 {
 	int i, j, k, len, f, digit;
 
