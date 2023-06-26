eca7562d14b0e59c04ceace5a7450a190b24c3b4
Stephen <mbuguagichimu@gmail.com>
2023-06-26 10:36:07 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _stoi - used to convert a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _stoi(char *s)
+{
+	int i, j, k, len, f, digit;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (j % 2)
+				digit = -digit;
+			k = k * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (k);
+}
+
