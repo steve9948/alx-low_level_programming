0e71a881588ff13fb9e5fa5ea2d6f2a7437a24be
Stephen <mbuguagichimu@gmail.com>
2023-06-26 11:20:24 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,58 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-#define PASSWORD_LENGTH 8
+#define PASSWORD_LENGTH 15
+#define EXPECTED_PASSWORD "Tada! Congrats"
 
-/**
- * generateRandomChar - Generates a random character
- *
- * Return: A random character
- */
 char generateRandomChar(void)
 {
-	int randomNum = rand() % 62;
+    int randomNum = rand() % 62;
 
-	if (randomNum < 26)
-		return 'A' + randomNum;
-	else if (randomNum < 52)
-		return 'a' + (randomNum - 26);
-	else
-		return '0' + (randomNum - 52);
+    if (randomNum < 26)
+        return 'A' + randomNum;
+    else if (randomNum < 52)
+        return 'a' + (randomNum - 26);
+    else
+        return '0' + (randomNum - 52);
 }
 
-/**
- * generateRandomPassword - Generates a random password
- * @password: The buffer to store the generated password
- *
- * This function generates a random password of length 8 by calling the
- * generateRandomChar() function and storing the characters in the password
- * buffer.
- */
 void generateRandomPassword(char *password)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-		password[i] = generateRandomChar();
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+        password[i] = generateRandomChar();
 
-	password[i] = '\0';
+    password[i] = '\0';
 }
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
+    char password[PASSWORD_LENGTH + 1];
 
-	srand(time(NULL));
-	generateRandomPassword(password);
+    srand(time(NULL));
+    generateRandomPassword(password);
 
-	printf("Generated Password: %s\n", password);
+    printf("%s\n", password);
 
-	return 0;
+    if (strcmp(password, EXPECTED_PASSWORD) == 0)
+        printf("Tada! Congrats\n");
+    else
+        printf("Wrong password\n");
+
+    return 0;
 }
 
