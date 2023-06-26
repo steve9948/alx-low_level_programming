970f7f883cbcc4d413e757f32d79d97c6012464c
Stephen <mbuguagichimu@gmail.com>
2023-06-26 11:17:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,32 +4,55 @@
 
 #define PASSWORD_LENGTH 8
 
-char generateRandomChar() {
-    int randomNum = rand() % 62;
-    if (randomNum < 26) {
-        return 'A' + randomNum;
-    } else if (randomNum < 52) {
-        return 'a' + (randomNum - 26);
-    } else {
-        return '0' + (randomNum - 52);
-    }
+/**
+ * generateRandomChar - Generates a random character
+ *
+ * Return: A random character
+ */
+char generateRandomChar(void)
+{
+	int randomNum = rand() % 62;
+
+	if (randomNum < 26)
+		return 'A' + randomNum;
+	else if (randomNum < 52)
+		return 'a' + (randomNum - 26);
+	else
+		return '0' + (randomNum - 52);
 }
 
-void generateRandomPassword(char* password) {
-    int i;
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = generateRandomChar();
-    }
-    password[PASSWORD_LENGTH] = '\0';
+/**
+ * generateRandomPassword - Generates a random password
+ * @password: The buffer to store the generated password
+ *
+ * This function generates a random password of length 8 by calling the
+ * generateRandomChar() function and storing the characters in the password
+ * buffer.
+ */
+void generateRandomPassword(char *password)
+{
+	int i;
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+		password[i] = generateRandomChar();
+
+	password[i] = '\0';
 }
 
-int main() {
-    srand(time(NULL));
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
 
-    char password[PASSWORD_LENGTH + 1];
-    generateRandomPassword(password);
+	srand(time(NULL));
+	generateRandomPassword(password);
 
-    printf("Generated Password: %s\n", password);
+	printf("Generated Password: %s\n", password);
 
-    return 0;
+	return 0;
 }
+
