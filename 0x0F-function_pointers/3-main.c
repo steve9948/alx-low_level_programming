d50c69b07b6b7a4a729bcebdf8bbd5cbbb70816d
Stephen <mbuguagichimu@gmail.com>
2023-07-19 10:17:18 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int i, j;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	i = atoi(argv[1]);
+	op = argv[2];
+	j = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && j == 0) ||
+	    (*op == '%' && j == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(i, j));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Prints the sum of two numbers.
+ * @a: First number.
+ * @b: second number.
+ *
+ * Return: Sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - prints the difference of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Prints the product of two numbers.
+ * @a: First number.
+ * @b: second number.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Prints the division of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Prints the remainder of the division of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: The remainder of the division of int a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
