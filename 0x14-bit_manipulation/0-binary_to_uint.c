095c6fe837d8e9b68ed67448783fd972a873386f
steve9948 <mbuguagichimu@gmail.com>
2023-08-04 04:17:58 -0700

    Resolved andrepaired question 0 on the null

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,30 +3,26 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string of 0 and 1 characters.
- * Return: The converted number, or 0 if invalid input.
+ * @b: Pointer to a string of 0 and 1 chars.
+ *
+ * Return: The converted number, or 0 if an error occurs.
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int idx = 0;
+	unsigned int result = 0;
 
-    if (b == NULL)
-        return 0;
+	if (b == NULL)
+		return (0);
 
-    while (b[idx] != '\0')
-    {
-        if (b[idx] == '0' || b[idx] == '1')
-        {
-            result = (result << 1) + (b[idx] - '0');
-            idx++;
-        }
-        else
-        {
-            return 0; /* Return 0 if invalid input */
-        }
-    }
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
 
-    return result;
+		result <<= 1;
+		result += *b - '0';
+		b++;
+	}
+
+	return (result);
 }
-
